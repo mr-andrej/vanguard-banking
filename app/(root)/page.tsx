4ef0e1af6af602ec518c0f505bc8fd0a5fba275b
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-10-16 15:54:26 +0200

    Added basic components to start with

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import HeaderBox from "@/components/headerBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+    const loggedIn = {firstName: "Jean-Claude"};
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1350.55}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 export default Home;
