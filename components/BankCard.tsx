b58b7ced5a4adfd19973a8af11a53b351329a2c6
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-10-19 15:05:10 +0200

    Implemented full routing for the sidebar (those links still lead nowhere though)

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -22,11 +22,11 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                                 {userName}
                             </h1>
                             <h2 className="text-12 font-semibold text-white">
-                                ** / **
+                                ●● / ●●
                             </h2>
                         </div>
                         <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                            **** **** **** <span className="text-16">{account.mask}</span>
+                            ●●●● ●●●● ●●●● <span className="text-16">{account.mask || "2134"}</span>
                         </p>
                     </article>
                 </div>
@@ -43,6 +43,7 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                         width={45}
                         height={32}
                         alt="mastercard"
+                        className="ml-5"
                     />
                 </div>
 
