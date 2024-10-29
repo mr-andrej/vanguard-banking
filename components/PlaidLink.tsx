b7b48034bbed0d1d3a2ddd90c432eb729258fd00
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-10-29 11:31:31 +0100

    Fixed Plaid popup not being a popup due to bad code

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,11 +1,12 @@
 import React, {useCallback, useEffect, useState} from "react";
-import {Button} from "@/components/ui/button";
+import {Button} from "./ui/button";
 import {PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink} from "react-plaid-link";
 import {useRouter} from "next/navigation";
 import {createLinkToken, exchangePublicToken} from "@/lib/actions/user.actions";
 
 const PlaidLink = ({user, variant}: PlaidLinkProps) => {
     const router = useRouter();
+
     const [token, setToken] = useState("");
 
     useEffect(() => {
@@ -38,9 +39,9 @@ const PlaidLink = ({user, variant}: PlaidLinkProps) => {
         <>
             {variant === "primary" ? (
                 <Button
-                    className="plaidlink-primary"
                     onClick={() => open()}
                     disabled={!ready}
+                    className="plaidlink-primary"
                 >
                     Connect bank
                 </Button>
@@ -56,4 +57,5 @@ const PlaidLink = ({user, variant}: PlaidLinkProps) => {
         </>
     );
 };
+
 export default PlaidLink;
