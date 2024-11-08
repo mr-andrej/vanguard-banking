afd6bd09762f5451fab6f55e00a5eeb637980fec
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-11-08 18:43:34 +0100

    Added the ability to transfer to other user's via their Plaid shareable ID

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import HeaderBox from "@/components/headerBox";
+import PaymentTransferForm from "@/components/PaymentTransferForm";
+import {getLoggedInUser} from "@/lib/actions/user.actions";
+import {getAccounts} from "@/lib/actions/bank.actions";
 
-const Transfer = () => {
+const Transfer = async () => {
+    const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({userId: loggedIn.$id});
+
+    if (!accounts) {
+        return ;
+    }
+
+    const accountsData = accounts?.data;
+
     return (
-        <div>Transfer</div>
+        <section className="payment-transfer">
+            <HeaderBox
+                title="Payment Transfer"
+                subtext="Please make sure to provide any relevant details related to the transfer."
+            />
+
+            <section className="size-full pt-5">
+                <PaymentTransferForm accounts={accountsData}/>
+            </section>
+        </section>
     );
 };
 export default Transfer;
