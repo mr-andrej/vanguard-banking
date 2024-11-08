cf6872dece1ea8eb3d3d032f9bc4ebad67992f36
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-11-08 16:45:20 +0100

    Fixed card component bug that made cards of different sizes

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -5,12 +5,12 @@ import {formatAmount} from "@/lib/utils";
 
 const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col w-full">
             <Link href="/" className="bank-card">
                 <div className="bank-card_content">
                     <div>
                         <h1 className="text-16 font-semibold text-white">
-                            {userName}
+                            {account.name}
                         </h1>
                         <p className="font-ibm-plex-serif font-black text-white">
                             {formatAmount(account.currentBalance)}
@@ -26,7 +26,7 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                             </h2>
                         </div>
                         <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                            ●●●● ●●●● ●●●● <span className="text-16">{account.mask || "2134"}</span>
+                            ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
                         </p>
                     </article>
                 </div>
@@ -43,20 +43,18 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
                         width={45}
                         height={32}
                         alt="mastercard"
-                        className="ml-5"
+                        className="ml-10"
                     />
                 </div>
 
                 <Image
                     src="/icons/lines.png"
-                    width={316}
+                    width={346}
                     height={190}
                     alt="lines"
                     className="absolute top-0 left-0"
                 />
             </Link>
-
-            {/* TODO: Add copy card digits feature */}
         </div>
     );
 };
