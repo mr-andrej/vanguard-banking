f8cac540088215591243b719d992b8aa301cbd65
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2024-11-12 17:58:57 +0100

    Fixed data type issue and now new notes are correctly created in appwrite

--- app/tools/notes/create/page.tsx
+++ app/tools/notes/create/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import NoteForm from "@/components/NoteForm";
+import {getLoggedInUser} from "@/lib/actions/user.actions";
 
-const Page = () => {
+const Page = async () => {
+    const loggedIn = await getLoggedInUser();
+
     return (
-        <section className="flex-center size-full max-sm:px-6">
-            <NoteForm type="create"/>
-        </section>
+        <div className="flex-center size-full max-sm:px-6">
+            <NoteForm loggedIn={loggedIn} />
+        </div>
     );
 };
 export default Page;
